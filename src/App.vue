d8dc6e7d1a6d253168ac567be7d14f5467b78f39
Anastasiia Chorna <achorn@softserveinc.com>
2020-07-27 19:26:50 +0300

    Read data from server -  jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,13 @@ export default {
   name: 'App',
   data() {
     return {
-      todos: [
-        { id: 1, title: 'Купить хлеб', completed: false },
-        { id: 2, title: 'Погулять с собакой', completed: false },
-        { id: 3, title: 'Забрать сумку с ремонта', completed: false }
-      ]
+      todos: []
     }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=8')
+        .then(response => response.json())
+        .then(json => this.todos = json)
   },
   components: {
     TodoList, AddTodo
