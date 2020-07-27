4fa97f0213ecd03de320a47133a2a967d0681735
Anastasiia Chornaya <nastyachornaya4@gmail.com>
2020-07-27 19:05:07 +0300

    First version of the Todo app
    Local list with tasks

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1>Todo List</h1>
+    <AddTodo
+      @addTodo="addNewTodo"
+    />
+    <hr>
+    <TodoList
+      v-bind:todoList="todos"
+      @remove-todo="removeTodo"
+    />
+  </div>
+</template>
+
+<script>
+
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        { id: 1, title: 'Купить хлеб', completed: false },
+        { id: 2, title: 'Погулять с собакой', completed: false },
+        { id: 3, title: 'Забрать сумку с ремонта', completed: false }
+      ]
+    }
+  },
+  components: {
+    TodoList, AddTodo
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addNewTodo(item) {
+      this.todos.push(item)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem
+              v-for="(t, i) in todoList"
+              :key="t.id"
+              v-bind:todo="t"
+              v-bind:index="i"
+              v-on:remove-item="removeElement"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+  import TodoItem from '@/components/TodoItem'
+  export default {
+    props: ['todoList'],
+    components: {
+      TodoItem
+    },
+    methods: {
+      removeElement(id) {
+        this.$emit('remove-todo', id)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
